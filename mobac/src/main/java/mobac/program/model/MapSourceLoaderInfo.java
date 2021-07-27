0019a28143e93b105085f28ec989b649c055d39f
r_x <r_x@users.sourceforge.net>
2021-07-27 13:37:32 +0000

    problem with constructor fixed if sourceFile is null
    git-svn-id: svn://svn.code.sf.net/p/mobac/code/trunk/MOBAC@2616 a036c9c4-4c23-4c51-82c3-8fdb5c987102

--- mobac/src/main/java/mobac/program/model/MapSourceLoaderInfo.java
+++ mobac/src/main/java/mobac/program/model/MapSourceLoaderInfo.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
  * Copyright (c) MOBAC developers
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
@@ -22,94 +22,99 @@ import java.util.LinkedList;
 
 public class MapSourceLoaderInfo {
 
-	public enum LoaderType {
-		MAPPACK("Mappack"), // map pack file
-		XML("Custom XML"), // custom map xml
-		BSH("BeanShell") // BeanShell script
-		;
+    public enum LoaderType {
+        MAPPACK("Mappack"), // map pack file
+        XML("Custom XML"), // custom map xml
+        BSH("BeanShell") // BeanShell script
+        ;
 
-		public final String displayName;
+        public final String displayName;
 
-		private LoaderType(String displayName) {
-			this.displayName = displayName;
-		}
-	};
+        private LoaderType(String displayName) {
+            this.displayName = displayName;
+        }
+    }
 
-	protected final LoaderType loaderType;
+    ;
 
-	protected final File sourceFile;
+    protected final LoaderType loaderType;
 
-	protected final long sourceFileLastModified;
-	protected final long sourceFileSize;
+    protected final File sourceFile;
 
-	protected final String revision;
+    protected final long sourceFileLastModified;
+    protected final long sourceFileSize;
 
-	public MapSourceLoaderInfo(LoaderType loaderType, File sourceFile) {
-		this(loaderType, sourceFile, null);
-	}
+    protected final String revision;
 
-	public MapSourceLoaderInfo(LoaderType loaderType, File sourceFile, String revision) {
-		super();
-		this.loaderType = loaderType;
-		this.sourceFile = sourceFile;
-		this.sourceFileLastModified = sourceFile.lastModified();
-		this.sourceFileSize = sourceFile.length();
-		this.revision = revision;
-	}
+    public MapSourceLoaderInfo(LoaderType loaderType, File sourceFile) {
+        this(loaderType, sourceFile, null);
+    }
 
-	public LoaderType getLoaderType() {
-		return loaderType;
-	}
+    public MapSourceLoaderInfo(LoaderType loaderType, File sourceFile, String revision) {
+        super();
+        this.loaderType = loaderType;
+        this.sourceFile = sourceFile;
+        if (sourceFile != null) {
+            this.sourceFileLastModified = sourceFile.lastModified();
+            this.sourceFileSize = sourceFile.length();
+        } else {
+            this.sourceFileLastModified = -1;
+            this.sourceFileSize = -1;
+        }
+        this.revision = revision;
+    }
 
-	public File getSourceFile() {
-		return sourceFile;
-	}
+    public LoaderType getLoaderType() {
+        return loaderType;
+    }
 
-	public Path getSourceFileRelativePath() {
-		Path mapSourceDir = Settings.getInstance().getMapSourcesDirectory().toPath();
-		return mapSourceDir.relativize(sourceFile.toPath());
-	}
+    public File getSourceFile() {
+        return sourceFile;
+    }
 
-	public long getSourceFileLastModified() {
-		return sourceFileLastModified;
-	}
+    public Path getSourceFileRelativePath() {
+        Path mapSourceDir = Settings.getInstance().getMapSourcesDirectory().toPath();
+        return mapSourceDir.relativize(sourceFile.toPath());
+    }
 
-	public long getSourceFileSize() {
-		return sourceFileSize;
-	}
+    public long getSourceFileLastModified() {
+        return sourceFileLastModified;
+    }
 
-	public String getRevision() {
-		return revision;
-	}
+    public long getSourceFileSize() {
+        return sourceFileSize;
+    }
 
-	/**
-	 * Check if the modification date or the file size of the source file has changed
-	 * 
-	 * @return <code>true</code> if a change was detected
-	 */
-	public boolean checkSourcesfileChanged() {
-		return (sourceFile.lastModified() != sourceFileLastModified) || (sourceFile.length() != sourceFileSize);
-	}
+    public String getRevision() {
+        return revision;
+    }
 
-	/**
-	 * This method gets a MapSource's path, relative to the /mapsources folder.
-	 * 
-	 * @return Path - relative path to the /mapsources folder, returns null if the file is directly in the /mapsources
-	 *         folder
-	 * 
-	 */
-	public String[] getRelativePath() {
-		File mapSourcesDir = Settings.getInstance().getMapSourcesDirectory();
-		File dir = sourceFile.getParentFile();
-		LinkedList<String> pathList = new LinkedList<>();
-		while (!mapSourcesDir.equals(dir)) {
-			pathList.offerFirst(dir.getName());
-			dir = dir.getParentFile();
-		}
-		if (pathList.size() == 0) {
-			return null;
-		} else {
-			return pathList.toArray(new String[pathList.size()]);
-		}
-	}
+    /**
+     * Check if the modification date or the file size of the source file has changed
+     *
+     * @return <code>true</code> if a change was detected
+     */
+    public boolean checkSourcesfileChanged() {
+        return (sourceFile.lastModified() != sourceFileLastModified) || (sourceFile.length() != sourceFileSize);
+    }
+
+    /**
+     * This method gets a MapSource's path, relative to the /mapsources folder.
+     *
+     * @return Path - relative path to the /mapsources folder, returns null if the file is directly in the /mapsources
+     * folder
+     */
+    public String[] getRelativePath() {
+        File mapSourcesDir = Settings.getInstance().getMapSourcesDirectory();
+        File dir = sourceFile.getParentFile();
+        LinkedList<String> pathList = new LinkedList<>();
+        while (!mapSourcesDir.equals(dir)) {
+            pathList.offerFirst(dir.getName());
+            dir = dir.getParentFile();
+        }
+        if (pathList.isEmpty()) {
+            return null;
+        }
+        return pathList.toArray(new String[pathList.size()]);
+    }
 }
