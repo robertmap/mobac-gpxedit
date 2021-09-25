c10541b25254cacf60dfd6ddfbbbf38e276c6042
r_x <r_x@users.sourceforge.net>
2021-09-25 12:27:42 +0000

    OSUtilities partially rewritten and improved
    git-svn-id: svn://svn.code.sf.net/p/mobac/code/trunk/MOBAC@2625 a036c9c4-4c23-4c51-82c3-8fdb5c987102

--- mobac/src/main/java/mobac/utilities/OSUtilities.java
+++ mobac/src/main/java/mobac/utilities/OSUtilities.java
@@ -1,26 +1,33 @@
 /*******************************************************************************
  * Copyright (c) MOBAC developers
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package mobac.utilities;
 
+import java.awt.Desktop;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FilenameFilter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import javax.naming.NameNotFoundException;
 import javax.swing.JOptionPane;
@@ -29,163 +36,103 @@ import org.apache.log4j.Logger;
 
 public class OSUtilities {
 
-	static Logger log = Logger.getLogger(OSUtilities.class);
+    static Logger log = Logger.getLogger(OSUtilities.class);
 
-	public enum OperatingSystem {
-		Windows, Linux, MacOs, MacOsX, Solaris, Unknown
-	};
+    public enum OperatingSystem {
+        Windows, Linux, MacOsX, Solaris, Unknown
+    }
 
-	public enum DesktopType {
-		Windows, Gnome, Kde, Unknown {
-			@Override
-			public String toString() {
-				return super.toString() + " (" + System.getProperty("sun.desktop") + ")";
-			}
-		}
-	};
+    public static final boolean IS_PLATFORM_OSX = isPlatformOsx();
 
-	public static OperatingSystem detectOs() {
-		String osName = System.getProperty("os.name").toLowerCase();
-		if (osName.indexOf("windows") > -1)
-			return OperatingSystem.Windows;
-		if (osName.indexOf("linux") > -1)
-			return OperatingSystem.Linux;
-		if (osName.indexOf("mac os x") > -1)
-			return OperatingSystem.MacOsX;
-		if (osName.indexOf("mac os") > -1)
-			return OperatingSystem.MacOs;
-		if (osName.indexOf("sunos") > -1)
-			return OperatingSystem.Solaris;
+    private static boolean isPlatformOsx() {
+        String os = System.getProperty("os.name");
+        return os != null && os.toLowerCase().startsWith("mac os x");
+    }
 
-		return OperatingSystem.Unknown;
-	}
+    public static OperatingSystem detectOs() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("windows")) {
+            return OperatingSystem.Windows;
+        }
+        if (osName.contains("linux")) {
+            return OperatingSystem.Linux;
+        }
+        if (osName.contains("mac os x")) {
+            return OperatingSystem.MacOsX;
+        }
 
-	public static DesktopType detectDesktopType() {
-		String desktopName = System.getProperty("sun.desktop");
-		if (desktopName == null)
-			return DesktopType.Unknown;
-		desktopName = desktopName.toLowerCase().trim();
-		if (desktopName.startsWith("windows"))
-			return DesktopType.Windows;
-		if (desktopName.startsWith("gnome"))
-			return DesktopType.Gnome;
-		if (desktopName.startsWith("kde"))
-			return DesktopType.Kde;
-		log.error("Unknown desktop type: " + desktopName);
-		return DesktopType.Unknown;
-	}
+        return OperatingSystem.Unknown;
+    }
 
-	public static void openFolderBrowser(File directory) throws NameNotFoundException {
-		if (!directory.isDirectory())
-			throw new NameNotFoundException("Directory does not exist or is not a directory");
-		String[] strCmd = null;
-		try {
-			String dirPath = directory.getCanonicalPath();
-			DesktopType dt = detectDesktopType();
-			switch (dt) {
-			case Windows:
-				strCmd = new String[] { "rundll32.exe", "url.dll,FileProtocolHandler", "\"" + dirPath + "\"" };
-				break;
-//			case Gnome:
-//				strCmd = new String[] { "nautilus", dirPath };
-//				break;
-//			case Kde:
-//				strCmd = new String[] { "konqueror", dirPath };
-//				break;
-			default:
-				OperatingSystem os = detectOs();
-				switch (os) {
-				case MacOsX:
-					strCmd = new String[] { "/usr/bin/open", dirPath };
-					break;
-				case Linux:
-					strCmd = new String[] { "/usr/bin/xdg-open", dirPath };
-					break;
-				default:
-					JOptionPane.showMessageDialog(null,
-							String.format(I18nUtils.localizedStringForKey("msg_environment_not_support"), dt, os),
-							I18nUtils.localizedStringForKey("msg_environment_not_support_title"),
-							JOptionPane.ERROR_MESSAGE);
-					return;
-				}
-			}
-			for (String s : strCmd) {
-				log.trace("exec/params: " + s);
-			}
-			Runtime.getRuntime().exec(strCmd);
-		} catch (Exception e) {
-			StringBuilder sb = new StringBuilder(512);
-			sb.append(I18nUtils.localizedStringForKey("msg_environment_failed_open_output"));
-			sb.append(e.getMessage());
-			sb.append(String.format(I18nUtils.localizedStringForKey("msg_environment_use_commond"), strCmd[0]));
-			for (int i = 1; i < strCmd.length; i++) {
-				sb.append(
-						String.format(I18nUtils.localizedStringForKey("msg_environment_commond_param"), i, strCmd[i]));
-			}
-			sb.append("</html>");
-			String msg = sb.toString();
-			JOptionPane.showMessageDialog(null, msg,
-					I18nUtils.localizedStringForKey("msg_environment_failed_open_output_title"),
-					JOptionPane.ERROR_MESSAGE);
-			log.error(msg, e);
-		}
+    public static void openFolderBrowser(File directory) throws NameNotFoundException {
+        if (!directory.isDirectory()) {
+            throw new NameNotFoundException("Directory does not exist or is not a directory");
+        }
+        try {
+            Desktop.getDesktop().open(directory.getAbsoluteFile().getCanonicalFile());
+        } catch (Exception e) {
+            StringBuilder sb = new StringBuilder(512);
+            sb.append(I18nUtils.localizedStringForKey("msg_environment_failed_open_output"));
+            sb.append(e.getMessage());
+            sb.append("</html>");
+            String msg = sb.toString();
+            JOptionPane.showMessageDialog(null, msg,
+                    I18nUtils.localizedStringForKey("msg_environment_failed_open_output_title"),
+                    JOptionPane.ERROR_MESSAGE);
+            log.error(msg, e);
+        }
 
-	}
+    }
 
-	/**
-	 * Reads the Linux distribution name (last line) from the first file that matches the pattern
-	 * 
-	 * <pre>
-	 * /etc/*-release
-	 * </pre>
-	 * 
-	 * @return Linux distrbution name or <code>null</code>
-	 */
-	public static String getLinuxDistributionName() {
-		try {
-			File etcDir = new File("/etc");
-			if (!etcDir.exists())
-				return null;
-			File[] files = etcDir.listFiles(new FilenameFilter() {
+    /**
+     * Reads the Linux distribution name (last line) from the first file that matches the pattern
+     *
+     * <pre>
+     * /etc/*-release
+     * </pre>
+     *
+     * @return Linux distribution name or <code>null</code>
+     */
+    public static String getLinuxDistributionName() {
+        try {
+            Path etcDir = Paths.get("/etc");
+            if (!Files.isDirectory(etcDir)) {
+                return null;
+            }
+            final Pattern pattern = Pattern.compile(".*-release");
+            List<Path> releaseFileList = Files.walk(etcDir).filter(f -> pattern.matcher(f.getFileName().toString()).matches()).collect(Collectors.toList());
+            if (releaseFileList.isEmpty()) {
+                return null;
+            }
+            Path releaseFile;
+            if (releaseFileList.size() > 1) {
+                Optional<Path> optional = releaseFileList.stream().filter(f -> f.getFileName().toString().contains("lsb")).findFirst();
+                if (optional.isPresent()) {
+                    releaseFile = optional.get();
+                } else {
+                    releaseFile = releaseFileList.get(0);
+                }
+            } else {
+                releaseFile = releaseFileList.get(0);
+            }
+            List<String> lines = Files.readAllLines(releaseFile);
+            String result = lines.stream().map(s -> {
+                s = s.trim();
+                int index = s.indexOf('=');
+                if (index > 0) {
+                    s = s.substring(index + 1);
+                }
+                if (s.startsWith("\"") && s.endsWith("\"")) {
+                    s = s.substring(1, s.length() - 2);
+                }
+                return s;
+            }).collect(Collectors.joining(", "));
+            return result;
+        } catch (Exception e) {
+            log.trace("Failed to read Linux release file", e);
+            return null;
+        }
+    }
 
-				Pattern pattern = Pattern.compile(".*-release");
 
-				public boolean accept(File dir, String name) {
-					return pattern.matcher(name).matches();
-				}
-			});
-			if (files.length == 0)
-				return null;
-			BufferedReader br = new BufferedReader(new FileReader(files[0]));
-			String result = null;
-			String line = null;
-			line = br.readLine();
-			while (line != null) {
-				result = line;
-				line = br.readLine();
-			}
-			br.close();
-			if (result != null) {
-				int index = result.indexOf('=');
-				if (index > 0)
-					result = result.substring(index + 1);
-				if (result.startsWith("\"") && result.endsWith("\""))
-					result = result.substring(1, result.length() - 2);
-			}
-			return result;
-		} catch (Exception e) {
-			log.trace("", e);
-			return null;
-		}
-	}
-
-	/**
-	 * Replies true if we are currently running on OSX
-	 * 
-	 * @return true if we are currently running on OSX
-	 */
-	public static boolean isPlatformOsx() {
-		String os = System.getProperty("os.name");
-		return os != null && os.toLowerCase().startsWith("mac os x");
-	}
 }
