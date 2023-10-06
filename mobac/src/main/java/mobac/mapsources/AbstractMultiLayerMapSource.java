f328c34084a65e9f53a78f735956e639a693d993
r_x <r_x@users.sourceforge.net>
2023-10-06 07:11:01 +0000

    [custom] multi-layer map source: only add successfully loaded layer images to the loaded list
    git-svn-id: svn://svn.code.sf.net/p/mobac/code/trunk/MOBAC@2756 a036c9c4-4c23-4c51-82c3-8fdb5c987102

--- mobac/src/main/java/mobac/mapsources/AbstractMultiLayerMapSource.java
+++ mobac/src/main/java/mobac/mapsources/AbstractMultiLayerMapSource.java
@@ -140,9 +140,9 @@ public abstract class AbstractMultiLayerMapSource implements InitializableMapSou
 			int maxSize = mapSpace.getTileSize();
 			for (MapSource layerMapSource : mapSources) {
 				BufferedImage layerImage = layerMapSource.getTileImage(zoom, x, y, loadMethod);
-				layerImages.add(layerImage);
 				if (layerImage != null) {
 					log.debug("Multi layer image loaded: " + layerMapSource + " " + x + " " + y + " " + zoom);
+					layerImages.add(layerImage);
 					int size = layerImage.getWidth();
 					if (size > maxSize) {
 						maxSize = size;
